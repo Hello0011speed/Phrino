e0cbce23744656a5c62fca2fd24cf75ef2a167e4
Hello0011speed <34991742+Hello0011speed@users.noreply.github.com>
2019-03-18 20:35:30 -0300

    Update client.js

--- client.js
+++ client.js
@@ -11,21 +11,6 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.channel.type === 'dm') return;
-    if (message.author.type === 'bot') return;
-    if (message.author.id !== '292337076395966492') return;
-    let args = message.content.split(' ');
-    let command = args[0];
-    let cmd = CH.getCommand(command);
-    if(!cmd) return;
-    
-    try{
-        cmd.run(client, message, args);
-    }catch(e){
-        console.log(e)
-    }
-});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
