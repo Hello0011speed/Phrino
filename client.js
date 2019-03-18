49fe1792d2e72f89ffd83c87dbba7e6ce5df1e30
Hello0011speed <34991742+Hello0011speed@users.noreply.github.com>
2019-03-18 20:27:04 -0300

    Update client.js

--- client.js
+++ client.js
@@ -1,14 +1,30 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const { CommandHandler } = require('djs-commands');
+const CH = new CommandHandler({
+    folder: __dirname + "/Commands/",
+    prefix: ['!']
+});
+
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('you are ugly');
-  	}
+    if (message.channel.type === 'dm') return;
+    if (message.author.type === 'bot') return;
+    if (message.author.id !== '292337076395966492') return;
+    let args = message.content.split(' ');
+    let command = args[0];
+    let cmd = CH.getCommand(command);
+    if(!cmd) return;
+    
+    try{
+        cmd.run(client, message, args);
+    }catch(e){
+        console.log(e)
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
