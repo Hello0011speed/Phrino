8e1dd73f9de823943d7ef3107a883a1fea08c53c
Hello0011speed <34991742+Hello0011speed@users.noreply.github.com>
2019-03-18 22:10:33 -0300

    Update client.js

--- client.js
+++ client.js
@@ -6,7 +6,17 @@ client.on('ready', () => {
     console.log('I am ready!');
     client.channels.get("557348230245908482").send("The bot was properly loaded ✅");
 });
+client.on('message', (message) => {
+    if(message.content.startsWith("-dm ")) {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("Insufficient Permissions.");
+    message.delete().catch(O_o=>{});
+        let messageToSend = message.content.split(" ").slice(2).join(" ");
+        let userToSend = message.mentions.users.first();
+        
 
+        userToSend.send(messageToSend);
+    }
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
