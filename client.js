7c0aa8445a1d0d0a6dae0e1026ca8ca395628ecd
Hello0011speed <34991742+Hello0011speed@users.noreply.github.com>
2019-03-19 16:03:54 -0300

    Update client.js

--- client.js
+++ client.js
@@ -1,16 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.commands = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-});
-
-
 client.on('ready', () => {
     console.log('I am ready!');
     client.channels.get("557348230245908482").send("The bot was properly loaded ✅");
