e21a8ce1b9dc45f1474b09017c7d6923411279b1
Hello0011speed <34991742+Hello0011speed@users.noreply.github.com>
2019-03-19 13:13:22 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,13 +3,6 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
   msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
 };
 
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: "User"
-};
-
 exports.help = {
   name: "ping",
   category: "Miscelaneous",
