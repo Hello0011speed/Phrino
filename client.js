7dabfb9be1646fb78af0a4689d666509930e4664
Hello0011speed <34991742+Hello0011speed@users.noreply.github.com>
2019-03-19 22:44:52 -0300

    Update client.js

--- client.js
+++ client.js
@@ -35,10 +35,7 @@ const embed = new Discord.RichEmbed()
     
 client.on('message', (message) => {
     if(message.content.startsWith("--dm ")) {   
-      const embed = new Discord.RichEmbed()
-            .setDescription("This command is only allowed for Phrino admins!")
-            .setColor('#d81111');
-    if (!admins.includes(message.author.id)) return message.channel.send(embed);
+    if (!admins.includes(message.author.id)) return message.channel.send("This command is only availeable for Premium Members and Staff. :wave");
     message.delete().catch(O_o=>{});
         let messageToSend = message.content.split(" ").slice(2).join(" ");
         let userToSend = message.mentions.users.first();
