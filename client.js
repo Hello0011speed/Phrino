d884dcccdf2c8fda80f107bcc95a864abbaa13a1
Hello0011speed <34991742+Hello0011speed@users.noreply.github.com>
2019-03-19 21:30:45 -0300

    Update client.js

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const config = require("./config.json");
-const adminID = ["358699020589465600", "359119668709163019", "292337076395966492"];
+const adminID = ["358699020589465600", "359119668709163019", "292337076395966492", "443159617028227083"];
 
 client.on('ready', () => {
     console.log('I am ready!');
