7d8be16eb43736e32219584f2d35d0396679928c
Hello0011speed <34991742+Hello0011speed@users.noreply.github.com>
2019-03-20 19:45:51 -0300

    Update client.js

--- client.js
+++ client.js
@@ -34,7 +34,7 @@ const embed = new Discord.RichEmbed()
     .setColor('#d81111');
     
 client.on('message', (message) => {
-    if(message.content.startsWith("--dm ")) {   
+    if(message.content.startsWith(".dm ")) {   
     if (!admins.includes(message.author.id)) return message.channel.send("This command is only availeable for Premium Members and Staff. :wave:");
     message.delete().catch(O_o=>{});
         let messageToSend = message.content.split(" ").slice(2).join(" ");
