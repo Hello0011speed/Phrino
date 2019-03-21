fa9fcb8e5faa822ab2c4387730726c219cf82e5f
DAKOTA <matujoa13@gmail.com>
2019-03-21 18:31:09 -0300

    Update client.js

--- client.js
+++ client.js
@@ -27,9 +27,7 @@ client.on('message', (message) =>{
             .setTitle("Phrino | Stats")
             .setAuthor("Phrino", "https://cdn.discordapp.com/icons/528818044668215299/6d1ccd655df1c562ef4f43835597fe10.png?")
             .setColor("#E51414")
-            .setDescription("Phrino Stats...")
-            .addField("Members: " + client.user.size)
-            .addField("Servers: " + client.guilds.size);
+            .setDescription("Members: " + client.users.size + "\n Servers:" + client.guilds.size);
 
   message.channel.send({embed});
     }
