5e4f4e046699cacad164cecbfb780306e6c7f26e
DAKOTA <matujoa13@gmail.com>
2019-03-21 19:17:58 -0300

    Update client.js

--- client.js
+++ client.js
@@ -48,7 +48,7 @@ client.on('message', (message) =>{
             .setTitle("Phrino | Verify")
             .setAuthor("Phrino", "https://cdn.discordapp.com/icons/528818044668215299/6d1ccd655df1c562ef4f43835597fe10.png?")
             .setColor("#E51414")
-            .setDescription("Welcome " + message.author + " you are verified as Phrino Staff!");
+            .setDescription("Welcome " + message.author.id + " you are verified as Phrino Staff!");
 
   message.channel.send({embedd});
     }
